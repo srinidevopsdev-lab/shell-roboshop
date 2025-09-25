611d268bb0d060fc45e2a895b574e3480cb791ff
Srinivas <srinidevopsdev@gmail.com>
2025-09-25 20:04:16 +0530

    roboshop

---
+++ mongodb.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE=$LOGS_FOLDER/$SCRIPT_NAME.log
+mkdir -p $LOGS_FOLDER
+echo "script started executed at: $(date)" | tee -a $LOG_FILE
+if [ $USERID -ne 0 ]; then
+    echo "Error::Please run this script as root user"
+    exit 1 # failure is othere than 0 Means it will stop here dont run furthur
+fi
+
+VALIDATE () {
+    if [ $1 -ne 0 ]; then
+        echo "Error: $2 installation is $R failure $N" | tee -a $LOG_FILE
+        exit 1 # 1 for failure
+    else
+        echo -e "$2 installation is $G successful $N" | tee -a $LOG_FILE
+    fi
+}
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Adding mongo repo"
+
+dnf install mongodb-org -y 
+VALIDATE $? "Installing mongodb"
+
+systemctl enable mongod 
+VALIDATE $? "Enabling mongodb"
+
+systemctl start mongod 
+VALIDATE $? "Starting mongodb"
