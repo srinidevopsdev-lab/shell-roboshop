6ec26d275409a34be0b298e184ae4bd547abc167
Srinivas <srinidevopsdev@gmail.com>
2025-09-25 06:22:30 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 AMI_ID="ami-09c813fb71547fc4f"
-SG_ID="sg-0d23d616835f667d2"
 
 for instance in $@ 
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids 
-    sg-0d23d616835f667d2 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids sg-0d23d616835f667d2 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances
     [0].InstanceId' --output text)
     
     if ($instance != 'frontend'); then
