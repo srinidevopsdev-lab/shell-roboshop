26df2f0e7958684e829f47dba62a6754d5f4b497
Srinivas <srinidevopsdev@gmail.com>
2025-09-26 22:15:07 +0530

    roboshop

--- mysql.sh
+++ mysql.sh
@@ -28,19 +28,14 @@ VALIDATE () {
     fi
 }
 
-
-dnf module disable redis -y  &>>$LOG_FILE
-VALIDATE $? "Disabling current module"
-dnf module enable redis:7 -y   &>>$LOG_FILE
-VALIDATE $? "Enabling latest module 7"
-dnf install redis -y   &>>$LOG_FILE
-VALIDATE $? "Installing redis module"
-sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf  
-VALIDATE $? "Allowing remote connections to redis"
-systemctl enable redis  &>>$LOG_FILE
-VALIDATE $? "Enabling redis"
-systemctl start redis   &>>$LOG_FILE
-VALIDATE $? "Starting redis"
+dnf install mysql-server -y
+VALIDATE $? "Installing mysql"
+systemctl enable mysqld
+VALIDATE $? "Enabling mysql"
+systemctl start mysqld 
+VALIDATE $? "Starting mysql" 
+mysql_secure_installation --set-root-pass RoboShop@1
+VALIDATE $? "Setting the default root password"
 
 END_TIME=$(date +%s)
 TOTAL_TIME=$(($END_TIME - $START_TIME))
