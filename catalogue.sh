71c7dc859168823efa936e17f244bd2ca40e6481
Srinivas <srinidevopsdev@gmail.com>
2025-09-26 06:10:39 +0530

    roboshop

--- catalogue.sh
+++ catalogue.sh
@@ -78,7 +78,7 @@ dnf install mongodb-mongosh -y  &>>$LOG_FILE
 VALIDATE $? "installing mongodb client"
 
 INDEX=$(mongosh mongodb.srinivasa.fun --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
-if [$INDEX -le 0 ]; then
+if [ $INDEX -le 0 ]; then
     mongosh --host $MONGODB_HOST </app/db/master-data.js  &>>$LOG_FILE
     VALIDATE $? "load catlogue products"
 else
